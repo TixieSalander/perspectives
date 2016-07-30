60b20872d06afc8fce4619cf2de7da3635e8c77f
Elie Meignan <eliepse.meignan@gmail.com>
2016-07-30 19:53:40 +0200

    Standart file strucure for index

---
+++ index.php
@@ -0,0 +1,288 @@
+<?php get_header() ?>
+
+<!-- Home timeline -->
+<section class="home">
+	<div class="container">
+		<!-- First grid module : post format articles -->
+		<div class="home-module">
+			<div class="home-module__grid-2-1 home-module__grid-1-m">
+				<div class="home-module__mainPart">
+					<!-- une tuile -->
+					<a class="home-moduleBig fpostArticle">
+						<div class="fpostArticle__image" style="background-image: url('/img/demo/Phallaina_promo_07-1024-524.jpg');"></div>
+						<div class="fpostArticle__content">
+							<h2 class="fpostArticle__title">Phallaina — La première bande défilée</h2>
+							<div class="fpostArticle__cat">
+								<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+									<path stroke="none" fill="#FFFFFF" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+								</svg>
+								Évènement
+							</div>
+						</div>
+					</a>
+					<div class="home-module__grid-2 home-module__grid-1-s">
+						<!-- une tuile -->
+						<a class="home-moduleMedium fpostArticle">
+							<div class="fpostArticle__image" style="background-image: url('/img/demo/Phallaina_promo_07-1024-524.jpg');"></div>
+							<div class="fpostArticle__content">
+								<h2 class="fpostArticle__title">Phallaina — La première bande défilée</h2>
+								<div class="fpostArticle__cat">
+									<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+										<path stroke="none" fill="#FFFFFF" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+									</svg>
+									Évènement
+								</div>
+							</div>
+						</a>
+						<!-- une tuile -->
+						<a class="home-moduleMedium fpostArticle">
+							<div class="fpostArticle__image" style="background-image: url('/img/demo/Phallaina_promo_07-1024-524.jpg');"></div>
+							<div class="fpostArticle__content">
+								<h2 class="fpostArticle__title">Phallaina — La première bande défilée</h2>
+								<div class="fpostArticle__cat">
+									<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+										<path stroke="none" fill="#FFFFFF" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+									</svg>
+									Évènement
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="home-module__sidePart home-module__sidePart-m home-module__sidePart-s">
+					<!-- une tuile -->
+					<a class="home-moduleHigh fpostArticle">
+						<div class="fpostArticle__image" style="background-image: url('/img/demo/Phallaina_promo_07-1024-524.jpg');"></div>
+						<div class="fpostArticle__content">
+							<h2 class="fpostArticle__title">Phallaina — La première bande défilée</h2>
+							<div class="fpostArticle__cat">
+								<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+									<path stroke="none" fill="#FFFFFF" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+								</svg>
+								Évènement
+							</div>
+						</div>
+					</a>
+					<!-- une tuile -->
+					<a class="home-moduleSmall fpostArticle">
+						<div class="fpostArticle__image" style="background-image: url('/img/demo/Phallaina_promo_07-1024-524.jpg');"></div>
+						<div class="fpostArticle__content">
+							<h2 class="fpostArticle__title">Slap It : Une lampe en forme de fessier s’allumant par pression</h2>
+							<div class="fpostArticle__cat">
+								<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+									<path stroke="none" fill="#FFFFFF" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+								</svg>
+								Évènement
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+
+
+		<!-- grid module mirror : post format dossier -->
+		<div class="home-module">
+			<div class="home-module__grid-1-2 home-module__grid-1-m">
+				<div class="home-module__sidePart home-module__sidePart-m home-module__sidePart-s">
+					<!-- une tuile -->
+					<a class="home-moduleHigh fpostDossier">
+						<h2 class="fpostDossier__title">Comment le Blackberry Passeport pourrait devenir un cas d'école</h2>
+						<p class="fpostDossier__extract">Avez vous entendu parlé il y a quelques mois, Blackberry avait annoncé deux modèles dont l’un se séparait du sacro-sain clavier AZERTY mécanique.</p>
+						<div class="fpostDossier__cat">
+							<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+								<path stroke="none" fill="#000000" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+							</svg>
+							Dossier
+						</div>
+					</a>
+					<!-- une tuile -->
+					<a class="home-moduleSmall fpostDossier">
+						<h2 class="fpostDossier__title">Comment le Blackberry Passeport pourrait devenir un cas d'école</h2>
+						<p class="fpostDossier__extract">Avez vous entendu parlé il y a quelques mois, Blackberry avait annoncé deux modèles dont l’un se séparait du sacro-sain clavier AZERTY mécanique.</p>
+						<div class="fpostDossier__cat">
+							<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+								<path stroke="none" fill="#000000" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+							</svg>
+							Dossier
+						</div>
+					</a>
+				</div>
+				<div class="home-module__mainPart">
+					<!-- une tuile -->
+					<a class="home-moduleBig fpostDossier">
+						<h2 class="fpostDossier__title">Comment le Blackberry Passeport pourrait devenir un cas d'école</h2>
+						<p class="fpostDossier__extract">Avez vous entendu parlé il y a quelques mois, Blackberry avait annoncé deux modèles dont l’un se séparait du sacro-sain clavier AZERTY mécanique.</p>
+						<div class="fpostDossier__cat">
+							<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+								<path stroke="none" fill="#000000" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+							</svg>
+							Dossier
+						</div>
+					</a>
+					<div class="home-module__grid-2 home-module__grid-1-s">
+						<!-- une tuile -->
+						<a class="home-moduleMedium fpostDossier">
+							<h2 class="fpostDossier__title">Comment le Blackberry Passeport pourrait devenir un cas d'école</h2>
+							<p class="fpostDossier__extract">Le Design émotionnel, ce petit plus qui nous fait passer du statut de consommateurs à celui d’amoureux d’une marque.</p>
+							<div class="fpostDossier__cat">
+								<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+									<path stroke="none" fill="#000000" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+								</svg>
+								Dossier
+							</div>
+						</a>
+						<!-- une tuile -->
+						<a class="home-moduleMedium fpostDossier">
+							<h2 class="fpostDossier__title">Comment le Blackberry Passeport pourrait devenir un cas d'école</h2>
+							<p class="fpostDossier__extract">Avez vous entendu parlé il y a quelques mois, Blackberry avait annoncé deux modèles dont l’un se séparait du sacro-sain clavier AZERTY mécanique.</p>
+							<div class="fpostDossier__cat">
+								<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+									<path stroke="none" fill="#000000" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+								</svg>
+								Dossier
+							</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
+		<!-- Third module : postformat video -->
+		<div class="home-module">
+			<div class="home-module__grid-2-1 home-module__grid-1-m">
+				<div class="home-module__mainPart">
+					<!-- une tuile -->
+					<a class="home-moduleBig fpostVideo" style="background-image: url('img/demo/miniature_loewy-1024-576.jpg')">
+						<div class="fpostVideo__content">
+							<h2 class="fpostVideo__title">L'homme qui créa la bouteille de Coca-Cola</h2>
+							<div class="fpostVideo__play">
+								<svg xmlns="http://www.w3.org/2000/svg" width="70" viewBox="-1 -1 62 62">
+									<path class="fpostVideo__playTriangle" fill="transparent" fill-rule="evenodd" stroke="#FFF" stroke-width="2" d="M30 60c16.57 0 30-13.43 30-30S46.57 0 30 0 0 13.43 0 30s13.43 30 30 30zm-8-41.01c0-1.65 1.136-2.274 2.545-1.387l16.91 10.646c1.405.884 1.41 2.317 0 3.204L24.545 42.1c-1.405.886-2.545.267-2.545-1.387V18.99z"/>
+								</svg>
+							</div>
+							<div class="fpostVideo__cat">
+								<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+									<path stroke="none" fill="#000000" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+								</svg>
+								Dossier
+							</div>
+						</div>
+					</a>
+					<div class="home-module__grid-2 home-module__grid-1-s">
+						<!-- une tuile -->
+						<a class="home-moduleMedium fpostVideo" style="background-image: url('img/demo/miniature_loewy-1024-576.jpg')">
+							<div class="fpostVideo__content">
+								<h2 class="fpostVideo__title">L'homme qui créa la bouteille de Coca-Cola</h2>
+								<div class="fpostVideo__play">
+									<svg xmlns="http://www.w3.org/2000/svg" width="70" viewBox="-1 -1 62 62">
+										<path class="fpostVideo__playTriangle" fill="transparent" fill-rule="evenodd" stroke="#FFF" stroke-width="2" d="M30 60c16.57 0 30-13.43 30-30S46.57 0 30 0 0 13.43 0 30s13.43 30 30 30zm-8-41.01c0-1.65 1.136-2.274 2.545-1.387l16.91 10.646c1.405.884 1.41 2.317 0 3.204L24.545 42.1c-1.405.886-2.545.267-2.545-1.387V18.99z"/>
+									</svg>
+								</div>
+								<div class="fpostVideo__cat">
+									<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+										<path stroke="none" fill="#000000" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+									</svg>
+									Dossier
+								</div>
+							</div>
+						</a>
+						<!-- une tuile -->
+						<a class="home-moduleMedium fpostVideo" style="background-image: url('img/demo/miniature_loewy-1024-576.jpg')">
+							<div class="fpostVideo__content">
+								<h2 class="fpostVideo__title">L'homme qui créa la bouteille de Coca-Cola</h2>
+								<div class="fpostVideo__play">
+									<svg xmlns="http://www.w3.org/2000/svg" width="70" viewBox="-1 -1 62 62">
+										<path class="fpostVideo__playTriangle" fill="transparent" fill-rule="evenodd" stroke="#FFF" stroke-width="2" d="M30 60c16.57 0 30-13.43 30-30S46.57 0 30 0 0 13.43 0 30s13.43 30 30 30zm-8-41.01c0-1.65 1.136-2.274 2.545-1.387l16.91 10.646c1.405.884 1.41 2.317 0 3.204L24.545 42.1c-1.405.886-2.545.267-2.545-1.387V18.99z"/>
+									</svg>
+								</div>
+								<div class="fpostVideo__cat">
+									<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+										<path stroke="none" fill="#000000" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+									</svg>
+									Dossier
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="home-module__sidePart home-module__sidePart-m home-module__sidePart-s">
+					<!-- une tuile -->
+					<a class="home-moduleHigh fpostVideo" style="background-image: url('img/demo/miniature_loewy-1024-576.jpg')">
+						<div class="fpostVideo__content">
+							<h2 class="fpostVideo__title">L'homme qui créa la bouteille de Coca-Cola</h2>
+							<div class="fpostVideo__play">
+								<svg xmlns="http://www.w3.org/2000/svg" width="70" viewBox="-1 -1 62 62">
+									<path class="fpostVideo__playTriangle" fill="transparent" fill-rule="evenodd" stroke="#FFF" stroke-width="2" d="M30 60c16.57 0 30-13.43 30-30S46.57 0 30 0 0 13.43 0 30s13.43 30 30 30zm-8-41.01c0-1.65 1.136-2.274 2.545-1.387l16.91 10.646c1.405.884 1.41 2.317 0 3.204L24.545 42.1c-1.405.886-2.545.267-2.545-1.387V18.99z"/>
+								</svg>
+							</div>
+							<div class="fpostVideo__cat">
+								<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+									<path stroke="none" fill="#000000" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+								</svg>
+								Dossier
+							</div>
+						</div>
+					</a>
+					<!-- une tuile -->
+					<a class="home-moduleSmall fpostVideo" style="background-image: url('img/demo/miniature_loewy-1024-576.jpg')">
+						<div class="fpostVideo__content">
+							<h2 class="fpostVideo__title">L'homme qui créa la bouteille de Coca-Cola</h2>
+							<div class="fpostVideo__play">
+								<svg xmlns="http://www.w3.org/2000/svg" width="70" viewBox="-1 -1 62 62">
+									<path class="fpostVideo__playTriangle" fill="transparent" fill-rule="evenodd" stroke="#FFF" stroke-width="2" d="M30 60c16.57 0 30-13.43 30-30S46.57 0 30 0 0 13.43 0 30s13.43 30 30 30zm-8-41.01c0-1.65 1.136-2.274 2.545-1.387l16.91 10.646c1.405.884 1.41 2.317 0 3.204L24.545 42.1c-1.405.886-2.545.267-2.545-1.387V18.99z"/>
+								</svg>
+							</div>
+							<div class="fpostVideo__cat">
+								<svg width="10px" height="11px" viewBox="0 5 10 11" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+									<path stroke="none" fill="#000000" fill-rule="evenodd" d="M1.43333333,5.83291667 L1.25041667,5 L8.74791667,5 L8.565,5.83291667 L1.43333333,5.83291667 L1.43333333,5.83291667 Z M0,8.33333333 L0.847916667,15 L9.12625,15 L10,8.33333333 L0,8.33333333 L0,8.33333333 Z M9.27291667,7.5 L9.4275,6.66666667 L0.570833333,6.66666667 L0.725416667,7.5 L9.27291667,7.5 L9.27291667,7.5 Z"></path>
+								</svg>
+								Dossier
+							</div>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
+		<!-- Pagination module -->
+		<div class="pagination">
+			<!-- More button -->
+			<a class="pagination__more" href="" title="Afficher les publications suivantes">Afficher plus</a>
+			<!-- Page number list -->
+			<ul class="pagination__list">
+				<li class="pagination__item">
+					<a class="pagination__itemContent" href="">«</a>
+				</li>
+				<li class="pagination__item">
+					<a class="pagination__itemContent" href="">1</a>
+				</li>
+				<li class="pagination__item">
+					<a class="pagination__itemContent" href="">2</a>
+				</li>
+				<li class="pagination__item">
+					<span class="pagination__itemContent pagination__itemContent--current">3</span>
+				</li>
+				<li class="pagination__item">
+					<a class="pagination__itemContent" href="">4</a>
+				</li>
+				<li class="pagination__item">
+					<a class="pagination__itemContent" href="">5</a>
+				</li>
+				<li class="pagination__item">
+					<span class="pagination__itemContent pagination__itemContent--desactivated">…</span>
+				</li>
+				<li class="pagination__item">
+					<a class="pagination__itemContent" href="">16</a>
+				</li>
+				<li class="pagination__item">
+					<a class="pagination__itemContent" href="">»</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</section>
+
+<!--<script src="/js/script.js"></script>-->
+
+<?php get_footer() ?>
