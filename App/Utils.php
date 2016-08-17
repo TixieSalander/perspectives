72c5577746013e6516eebf904c2b0c62fc78e7ff
Elie Meignan <eliepse.meignan@gmail.com>
2016-08-17 22:17:44 +0800

    Increase getWPMenu flexibility

--- App/Utils.php
+++ App/Utils.php
@@ -12,25 +12,27 @@ class Utils
 
 		$locations = get_nav_menu_locations();
 
-		if (isset($locations[ $menu_name ]))
+		if (isset($locations[ $menu_name ]) && $locations[ $menu_name ] > 0)
 			$menu_id = $locations[ $menu_name ];
 		else
 			return false;
 
 		$items = wp_get_nav_menu_items($menu_id);
 
-		$html = "<ul class=\"$ul_class\">";
+		$html = $ul_class === false ? '' : "<ul class=\"$ul_class\">";
 
 		foreach ($items as $item) {
 
 			$title = $item->title;
 			$url = $item->url;
 
-			$html .= "<li class=\"$li_class\"><a href=\"$url\" class=\"$a_class\">$title</a></li>";
+			$html .= $li_class === false ? '' : "<li class=\"$li_class\">";
+			$html .= "<a href=\"$url\" class=\"$a_class\">$title</a>";
+			$html .= $li_class === false ? '' : "</li>";
 
 		}
 
-		$html .= "</ul>";
+		$html .= $ul_class === false ? '' : "</ul>";
 
 		return $html;
 
