42b55ed2f14a33f46e29a368753428a46d09f1df
Elie Meignan <eliepse.meignan@gmail.com>
2016-08-18 14:05:59 +0800

    Add socials manage possibilities

---
+++ config/global.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+
+	'rss_type' => 'rss2',
+
+	'socials_display' => [
+
+		'twitter' => [
+			'link_title' => 'Profil Twitter',
+			'img_file'   => 'ico-twitter.svg',
+			'img_alt'    => 'Twitter',
+		],
+
+		'youtube' => [
+			'link_title' => 'Chaine Youtube',
+			'img_file'   => 'ico-youtube.svg',
+			'img_alt'    => 'Youtube',
+		],
+
+		'instagram' => [
+			'link_title' => 'Profil Instagram',
+			'img_file'   => 'ico-instagram.svg',
+			'img_alt'    => 'Instagram',
+		],
+
+		'facebook' => [
+			'link_title' => 'Page Facebook',
+			'img_file'   => 'ico-facebook.svg',
+			'img_alt'    => 'Facebook',
+		],
+
+		'rss' => [
+			'link_title' => 'Flux RSS',
+			'img_file'   => 'ico-rss.svg',
+			'img_alt'    => 'RSS',
+		],
+
+	],
+];
