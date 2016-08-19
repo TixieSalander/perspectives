4550a17b77c4837b11f3ed5f2d1dc180b616ce09
Elie Meignan <eliepse.meignan@gmail.com>
2016-08-19 14:27:01 +0800

    Add archive

--- loop.php
+++ loop.php
@@ -9,17 +9,17 @@ $cardFactory = PostTypesCardFactory::getInstance();
 	<section class="articleList">
 		<div class="articleList__head">
 			<hr/>
-			<?php if(is_search()) : ?>
+			<?php if (is_search()) : ?>
 				<span class="articleList__smallTitle">Résultat de : </span>
 				<span class="articleList__bigTitle"><?= the_search_query(); ?></span>
 			<?php else: ?>
 				<span class="articleList__smallTitle">Articles du mois de : </span>
-				<span class="articleList__bigTitle">Janvier 2015</span>
+				<span class="articleList__bigTitle"><?= ucfirst(get_the_date('F Y')); ?></span>
 			<?php endif; ?>
 		</div>
 		<div class="articleList__grid-list articleList__grid-list-m articleList__grid-list-s">
 			<?php
-			if(have_posts()) {
+			if (have_posts()) {
 				while ($post = $cardFactory->getCard()) {
 					echo $post;
 				}
