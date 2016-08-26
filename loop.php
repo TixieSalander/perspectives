0e3ef74ea317e1d713baca3727abe491cad7bfe0
Elie Meignan <eliepse.meignan@gmail.com>
2016-08-26 17:55:23 +0800

    Modify the loop

--- loop.php
+++ loop.php
@@ -5,29 +5,12 @@ use App\PostTypesCardFactory;
 $cardFactory = PostTypesCardFactory::getInstance();
 
 ?>
-<div class="container">
-	<section class="articleList">
-		<div class="articleList__head">
-			<hr/>
-			<?php if (is_search()) : ?>
-				<span class="articleList__smallTitle">Résultat de : </span>
-				<span class="articleList__bigTitle"><?= the_search_query(); ?></span>
-			<?php else: ?>
-				<span class="articleList__smallTitle">Articles du mois de : </span>
-				<span class="articleList__bigTitle"><?= ucfirst(get_the_date('F Y')); ?></span>
-			<?php endif; ?>
-		</div>
-		<div class="articleList__grid-list articleList__grid-list-m articleList__grid-list-s">
-			<?php
-			if (have_posts()) {
-				while ($post = $cardFactory->getCard()) {
-					echo $post;
-				}
-			}
-			?>
-		</div>
-
-		<?= include 'pagination.php'; ?>
-
-	</section>
+<div class="articleList__grid-list articleList__grid-list-m articleList__grid-list-s">
+	<?php
+	if (have_posts()) {
+		while ($post = $cardFactory->getCard()) {
+			echo $post;
+		}
+	}
+	?>
 </div>
