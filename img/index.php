025126ce9be00221495f3af860d2cb4809e4c824
Elie Meignan <eliepse.meignan@gmail.com>
2016-09-04 10:05:21 +0200

    Reformat code a little

--- img/index.php
+++ img/index.php
@@ -19,6 +19,31 @@ function send403()
 	exit;
 }
 
+function send304($tsstring, $type)
+{
+	header('HTTP/1.1 304 Not Modified');
+	header("Last-Modified: $tsstring");
+	header('Content-Transfer-Encoding: binary');
+	header("Content-Type: image/$type");
+	exit();
+}
+
+function send200($abs_path, $tsstring, $type, $modifiedAt, $etag, $img_expire, $cache_file_size)
+{
+	header("Cache-Control: public, max-age=" . $img_expire, true);
+	header('Last-Modified: ' . $tsstring, true);
+	header('Pragma: public', true);
+	header('Content-Transfer-Encoding: binary', true);
+	header('Content-Length: ' . $cache_file_size, true);
+	header('Date: ' . gmdate('D, d M Y H:i:s ', time()), true);
+	header('Expires: ' . $modifiedAt->add(new DateInterval('PT' . $img_expire . 'S'))->format('rfc1123'), true);
+	header("Content-Type: image/$type", true);
+	header("Tag: {$etag}", true);
+	$file = fopen($abs_path, 'r');
+	fpassthru($file);
+	exit;
+}
+
 if (empty($_GET['id'])
 	|| !preg_match('/^[a-zA-Z0-9.]{23}\.[a-zA-Z]+$/', $_GET['id'])
 ) {
@@ -34,12 +59,12 @@ $imgCacheRef_content = $dataCache->read("img-cache", false, CacheManager::$_no_d
 $imgCacheRefs = json_decode($imgCacheRef_content, true);
 
 foreach ($imgCacheRefs as $id => $url) {
-	
+
 	if ($id === $img_ref) {
 		response($url, $id);
 		exit;
 	}
-	
+
 }
 
 /**
@@ -49,52 +74,31 @@ foreach ($imgCacheRefs as $id => $url) {
 function response($cacheUrl, $cacheFilename)
 {
 	global $img_expire, $imgCache;
-	
-	$content = $imgCache->readOrCacheUrl($cacheFilename, $cacheUrl, $img_expire, CacheManager::$_no_delete);
-	
-	
+
+	$imgCache->readOrCacheUrl($cacheFilename, $cacheUrl, $img_expire, CacheManager::$_no_delete);
+
 	$cache_file = $imgCache->getFileCache($cacheFilename);
-	
-	if (!$cache_file->isFileExist()) {
+
+	if (!$cache_file->isFileExist())
 		send404();
-	}
-	
 	$modifiedAt = empty($cache_file->getModifiedAt()) ? new DateTime() : $cache_file->getModifiedAt();
-	
+
 	$abs_path = $cache_file->getAbsolutePath();
 	$cache_file_size = filesize($abs_path);
 	$type = pathinfo($cacheFilename, PATHINFO_EXTENSION);
-	
+
 	$tsstring = $modifiedAt->format('rfc1123');
 	$etag = md5($cacheFilename . $modifiedAt->getTimestamp());
-	
+
 	$if_modified_since = isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) ? $_SERVER['HTTP_IF_MODIFIED_SINCE'] : false;
 	$if_none_match = isset($_SERVER['HTTP_IF_NONE_MATCH']) ? $_SERVER['HTTP_IF_NONE_MATCH'] : false;
-	
-	if ((($if_none_match && $if_none_match == $etag) || (!$if_none_match))
+
+	if ((($if_none_match && $if_none_match == $etag) || !$if_none_match)
 		&& ($if_modified_since && $if_modified_since == $tsstring)
 	) {
-		header('HTTP/1.1 304 Not Modified');
-		header("Last-Modified: $tsstring");
-		header('Content-Transfer-Encoding: binary');
-		header("Content-Type: image/$type");
-		exit();
+		send304($tsstring, $type);
 	} else {
-		$file = fopen($abs_path, 'r');
-//		echo $content;
-		header("Cache-Control: public, max-age=" . $img_expire, true);
-		header('Last-Modified: ' . $tsstring, true);
-		header('Pragma: public', true);
-		header('Content-Transfer-Encoding: binary', true);
-		header('Content-Length: ' . $cache_file_size, true);
-		header('Date: ' . gmdate('D, d M Y H:i:s ', time()), true);
-		header('Expires: ' . $modifiedAt->add(new DateInterval('PT' . $img_expire . 'S'))->format('rfc1123'), true);
-		header("Content-Type: image/$type", true);
-		header("Tag: {$etag}", true);
-//		echo "image/$type<br/>";
-//		echo $modifiedAt->add(new DateInterval('PT' . $img_expire . 'S'))->format('rfc1123');
-		fpassthru($file);
-		exit();
+		send200($abs_path, $tsstring, $type, $modifiedAt, $etag, $img_expire, $cache_file_size);
 	}
 }
 
